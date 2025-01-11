4e1c156e8db32144f0a10280b75a85e1cb916d1f
Vishma-007 <4al20ai049vishma@gmail.com>
2025-01-11 09:45:20 +0530

    Delete src directory

--- src/main/java/com/ust/Spring_Reactive/Repository/ProjectRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ust.Spring_Reactive.Repository;
-
-import com.ust.Spring_Reactive.entity.Project;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface ProjectRepo extends ReactiveMongoRepository<Project,String> {
-}
